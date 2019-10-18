be6febd851f1e5a868a9ec9052ada3d8cfa8fde4
teodormeurling <teodor.meurling@gmail.com>
2019-10-18 21:51:57 +0200

    logo height fix
    self explanatory

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./Header.css";
-
+import logo from "../../img/logo.png";
 const Header = () => {
     return (
         <div className="Header">
         	<div>Home</div>
-        	<img src="../../img/logo.png" alt="logo"/>
+        	<img className="logo" src={logo} alt="logo"/>
         	<div>Socials</div>
         </div>
     );
