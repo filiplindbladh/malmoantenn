0a1014e3d5d5c1e41454eda8d5c42ee42dde8bd2
Filip Lindbladh <filip.lindbladh@beamonpeople.se>
2019-10-19 15:04:20 +0200

    added nav to Menu and some layout adjustments in Header

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import "./Menu.css";
 import logo from "../../img/logo.png";
+import { Link } from "react-router-dom";
 
 const Menu = () => {
     return (
-        <div className="Menu">
-            <div>Home</div>
-            <img className="logo" src={logo} alt="logo" />
+        <nav className="Menu">
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+            </ul>
+            <Link to="/">
+                <img className="logo" src={logo} alt="logo" />
+            </Link>
             <div>Socials</div>
-        </div>
+        </nav>
     );
 };
 
