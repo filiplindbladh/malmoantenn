84039fc9d8d40349a5d2e73993d35cc1a928690c
Filip Lindbladh <filip.lindbladh@beamonpeople.se>
2019-10-27 21:01:39 +0100

    added eventCard

---
+++ src/components/EventCard/EventCard.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "./EventCard.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendar, faClock } from "@fortawesome/free-solid-svg-icons";
+import classNames from "classnames";
+
+const EventCard = ({
+    picture,
+    title,
+    startDate,
+    startTime,
+    endTime,
+    description,
+    isLink,
+}) => {
+    let classes = classNames({
+        EventCard: true,
+        Highlighted: isLink,
+    });
+    return (
+        <div className={classes}>
+            {picture && (
+                <img
+                    className="EventCard-image"
+                    src={picture}
+                    alt={title}
+                ></img>
+            )}
+            <div className="EventCard-info">
+                <p className="EventCard-date ">
+                    <span>
+                        <FontAwesomeIcon size="1x" icon={faCalendar} />
+                        {startDate.slice(0, 10).replace(/-/g, ".")}
+                    </span>
+                    &nbsp;
+                    <span className="EventCard-time">
+                        <FontAwesomeIcon size="1x" icon={faClock} />
+                        {startTime}
+                        &nbsp;-&nbsp;
+                        {endTime}
+                    </span>
+                </p>
+                <h3 className="Heading-medium">{title}</h3>
+                {description && (
+                    <p>{description.replace(/<\/?[^>]+(>|$)/g, "")}</p>
+                )}
+            </div>
+        </div>
+    );
+};
+export default EventCard;
