624a33d5d79c3e5dce6a7cb1dc3a662afd20138f
Filip Lindbladh <filip.lindbladh@beamonpeople.se>
2019-11-10 20:00:38 +0100

    added LivePlayer component

---
+++ src/components/LivePlayer/LivePlayer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./LivePlayer.css";
+
+const LivePlayer = () => {
+    return (
+        <figure className="LivePlayer">
+            <figcaption>Tune in</figcaption>
+            <audio controls src="https://s2.radio.co/se9588efb3/listen">
+                Your browser does not support the
+                <code>audio</code> element.
+            </audio>
+        </figure>
+    );
+};
+
+export default LivePlayer;
